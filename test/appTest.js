e812016b033d79a09bd5b67cba7c5a58d4e4e93a
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-06 12:18:39 -0700

    adds testing for random index and intial sorting

--- test/appTest.js
+++ test/appTest.js
@@ -2,5 +2,18 @@ const assert = require('chai').assert;
 const app = require('../app');
 
 describe('App', function(){
-  
+  describe('randomIndex()', function(){
+    it('should return random index < array length', function(){
+      randomIndex = app.randomIndex(5);
+      assert.isAbove(randomIndex, -1);
+      assert.isBelow(randomIndex, 5);
+    });
+  });
+  describe('initialSort()', function(){
+    it('should sort an array given array and pivot index', function(){
+      original = [9, 8, 5, 6];
+      sorted = app.initialSort(original, 3);
+      assert.equal(sorted, [5, 6, 9, 8]);
+    });
+  });
 })
