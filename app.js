f5ad73819377d8d175aadec088919c9a38bb552c
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-09 10:04:00 -0700

    refactors bubble sort to take array of any length

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 module.exports = {
   sort: function(unsorted) {
-    let swap = null;
-    unsorted.forEach((el, i) => {
-      if (unsorted[i] > unsorted[i+1]) {
-        swap = unsorted[i];
-        unsorted[i] = unsorted[i+1];
-        unsorted[i+1] = swap;
-      }
-      return unsorted;
-    });
+    let swapped;
+    do {
+      swapped = false;
+      let swap = null;
+      unsorted.forEach((el, i) => {
+        if (unsorted[i + 1] < unsorted[i]) {
+          swap = unsorted[i];
+          unsorted[i] = unsorted[i + 1];
+          unsorted[i + 1] = swap;
+          swapped = true;
+        }
+      });
+    } while (swapped);
     return unsorted;
   }
 }
--- test/appTest.js
+++ test/appTest.js
@@ -8,4 +8,11 @@ describe('App', function(){
   it('sorts an array of odd length', function(){
     assert.deepEqual(app.sort([3, 1, 2]), [1, 2, 3]);
   });
+  it('sorts a longer array', function(){
+    assert.deepEqual(app.sort([ 5, 4, 3, 2, 1 ]), [ 1, 2, 3, 4, 5 ]);
+  });
+  it('sorts other arrays', function(){
+    assert.deepEqual(app.sort([ 5, 1, 4, 2, 8 ]), [ 1, 2, 4, 5, 8 ]);
+    assert.deepEqual(app.sort([ 12, 3, 7, 8, 2 ]), [ 2, 3, 7, 8, 12 ]);
+  });
 })
